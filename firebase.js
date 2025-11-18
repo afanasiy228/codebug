f1beb7e723a6707d970b6e648b2584969f119426
afanasiy228 <109966000+afanasiy228@users.noreply.github.com>
2025-11-18 23:46:17 +0300

    fwfrf

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// === Firebase INIT (v8) === //
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDkQTqt1Hcg7H-a4rVl88dCbdJkDEiPDEg",
+    authDomain: "codebug-47347.firebaseapp.com",
+    databaseURL: "https://codebug-47347-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "codebug-47347",
+    storageBucket: "codebug-47347.firebasestorage.app",
+    messagingSenderId: "1000157426060",
+    appId: "1:1000157426060:web:4350f2d9f3db75e407e6a9"
+};
+
+// Инициализация
+firebase.initializeApp(firebaseConfig);
+
+// ГЛОБАЛЬНАЯ переменная
+window.db = firebase.database();
