71d0317e8db6f581b422c6ef1569c0d9500eb40d
afanasiy228 <109966000+afanasiy228@users.noreply.github.com>
2025-11-20 12:42:54 +0300

    text

--- tasks/2/code.cpp
+++ tasks/2/code.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main() {
-    int a, b, c;
-    cin >> a >> b; 
-    
-    int mn = a;
-    if (b < mn) mn = b;
-    if (c < mn) mn = c;
-    cout << mn;
+    long long n;
+    cin >> n;
+
+    int cnt = 0;
+    while (n > 0) {
+        cnt++;
+        n /= 10;
+    }
+    cout << cnt;
 }
