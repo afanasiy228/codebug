66bffc9c81a07d0c0c0e379025009f5307974bcb
afanasiy228 <109966000+afanasiy228@users.noreply.github.com>
2025-11-20 11:09:33 +0300

    text

---
+++ tasks/2/code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+  int a,b;
+  cin >> a >> b;
+  cout << a - b;
+  return 0;
+}
