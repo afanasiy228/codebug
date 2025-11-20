61e01ba650fed8d19ae63c3dbf0181038c46ed5b
afanasiy228 <109966000+afanasiy228@users.noreply.github.com>
2025-11-20 15:22:23 +0300

    text

--- sol.cpp
+++ sol.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c; 
-    
-    int mn = a;
-    if (b < mn) mn = b;
-    if (c < mn) mn = c;
-    cout << mn;
-}
+// Загружаю code.cpp...
